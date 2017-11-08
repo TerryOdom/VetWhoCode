fa12c84120c763de7456d61baf5d4e44a0c28ce6
TerryOdom <tech.junkie@outlook.com>
2017-11-07 22:13:10 -0500

    Delete if-statement.js

--- homework8/if-statement.js
+++
@@ -1,6 +0,0 @@
-var fruit = 'orange';
-if (fruit.legth > 5) {
-    console.log("The fruit name has five characters or less.")
-} else {
-    console.log("The fruit name has more than five characters.")
-}
